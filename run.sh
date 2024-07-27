0021badc89852b135e80648db8895915714e6b34
user475289 <shi222.wf+2nd@gmail.com>
2024-07-27 11:42:40 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python3 run_misc.py --dataset clic --lic_model cheng --model_desc cheng_vgg0.5_mse0.5 \
---df /root/shared_smurai/df/clic_llava_1.6_vicuna.csv --path /root/shared_smurai/lic_weights/cheng/Cheng2020Attention_3.0_mse0.5_vgg0.5_checkpoint_best_loss.pth.tar \
---mode tuned_net --save_path /root/shared_smurai/MISC_outputs/CLIC_cheng_vgg0.5_mse0.5 \
---from_encoded /root/shared_smurai/MISC_outputs/clic_encoded_cheng_vgg0.5_mse0.5.pickle
+--df df/clic_llava_1.6_vicuna.csv --path lic_weights/cheng/Cheng2020Attention_3.0_mse0.5_vgg0.5_checkpoint_best_loss.pth.tar \
+--mode tuned_net --save_path MISC_outputs/CLIC_cheng_vgg0.5_mse0.5 \
+--from_encoded MISC_outputs/clic_encoded_cheng_vgg0.5_mse0.5.pickle
 
